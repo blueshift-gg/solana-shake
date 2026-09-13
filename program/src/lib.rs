407bb957165f93085d779f7d37d38b77547be220
L0STE <leonardopio.donatacci@gmail.com>
2026-09-13 19:21:42 +0200

    refactor: split example instructions into handlers

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -14,18 +14,59 @@ fn process_instruction(_: &Pubkey, _: &[AccountInfo], data: &[u8]) -> ProgramRes
     let (&tag, data) = data
         .split_first()
         .ok_or(ProgramError::InvalidInstructionData)?;
+    match tag {
+        0 => shake128_hash(data),
+        1 => shake256_hash(data),
+        2 => turboshake128_hash(data),
+        3 => turboshake256_hash(data),
+        4 => shake128_hashv(data),
+        5 => shake256_hashv(data),
+        6 => turboshake128_hashv(data),
+        7 => turboshake256_hashv(data),
+        _ => Err(ProgramError::InvalidInstructionData),
+    }
+}
+
+fn shake128_hash(data: &[u8]) -> ProgramResult {
+    set_return_data(&Shake128::hash::<32>(data));
+    Ok(())
+}
+
+fn shake256_hash(data: &[u8]) -> ProgramResult {
+    set_return_data(&Shake256::hash::<32>(data));
+    Ok(())
+}
+
+fn turboshake128_hash(data: &[u8]) -> ProgramResult {
+    set_return_data(&TurboShake128::hash::<32, 0x1f>(data));
+    Ok(())
+}
+
+fn turboshake256_hash(data: &[u8]) -> ProgramResult {
+    set_return_data(&TurboShake256::hash::<32, 0x1f>(data));
+    Ok(())
+}
+
+fn shake128_hashv(data: &[u8]) -> ProgramResult {
     let (left, right) = data.split_at(data.len() / 2);
-    let output = match tag {
-        0 => Shake128::hash::<32>(data),
-        1 => Shake256::hash::<32>(data),
-        2 => TurboShake128::hash::<32, 0x1f>(data),
-        3 => TurboShake256::hash::<32, 0x1f>(data),
-        4 => Shake128::hashv::<32>(&[left, right]),
-        5 => Shake256::hashv::<32>(&[left, right]),
-        6 => TurboShake128::hashv::<32, 0x1f>(&[left, right]),
-        7 => TurboShake256::hashv::<32, 0x1f>(&[left, right]),
-        _ => return Err(ProgramError::InvalidInstructionData),
-    };
-    set_return_data(&output);
+    set_return_data(&Shake128::hashv::<32>(&[left, right]));
+    Ok(())
+}
+
+fn shake256_hashv(data: &[u8]) -> ProgramResult {
+    let (left, right) = data.split_at(data.len() / 2);
+    set_return_data(&Shake256::hashv::<32>(&[left, right]));
+    Ok(())
+}
+
+fn turboshake128_hashv(data: &[u8]) -> ProgramResult {
+    let (left, right) = data.split_at(data.len() / 2);
+    set_return_data(&TurboShake128::hashv::<32, 0x1f>(&[left, right]));
+    Ok(())
+}
+
+fn turboshake256_hashv(data: &[u8]) -> ProgramResult {
+    let (left, right) = data.split_at(data.len() / 2);
+    set_return_data(&TurboShake256::hashv::<32, 0x1f>(&[left, right]));
     Ok(())
 }
